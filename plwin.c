cdefdc13632d12a980405be6774600ffb3b79543
Ingemar <ingemar@lean.eiscat.se>
2021-10-04 11:33:19 +0200

    pyface

--- plwin.c
+++ plwin.c
@@ -611,3 +611,24 @@ void matface(int *par,int *nin,double *in_r,double *in_i,
 	}
 	free(in); free(out);
 }
+
+typedef struct {
+	double	re;
+	double	im;
+}	DSPCMPLXDBL;
+
+void pyface(int *par,int nin,DSPCMPLXDBL *in_r, DSPCMPLX *out, double *upar) {
+	ulong nb=par[0]+par[4]*par[7];
+	float *fpar=NULL;
+	int i;
+	DSPCMPLXSHORT *in;
+	in=(DSPCMPLXSHORT *)malloc(nin*sizeof(DSPCMPLXSHORT));
+	for(i=0;i<nin;i++) {
+		in[i].re=in_r[i].re;
+		in[i].im=in_r[i].im;
+	}
+	decoder_6(nb,par,0,fpar,in,out,upar);
+	free(in);
+}
+
+
